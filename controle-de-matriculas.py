71b1637e40f62483125ed739fd1f043b64b8cb99
Mecduda99 <rm98760@fiap.com.br>
2023-05-04 22:46:28 -0300

    "funçoes e condicional adicionados"

--- controle-de-matriculas.py
+++ controle-de-matriculas.py
@@ -1,8 +1,4 @@
-matriculas = {1: ["RM", "Nome",18, "Curso", "Turma"], 2: ["RM1", "Nome1",19, "Curso1", "Turma1"]}
-
-def add_matricula():
-    id = list(matriculas.keys())
-    matriculas[id[len()-1]+1] = [input("Digite o RM:"), input("Digite o nome:"), int(input("Digite a idade do aluno:")), input("Digite o curso:"), input("Digite a Turma:")]
+matriculas = {1: ["98760", "Nome",18, "Curso", "Turma"], 2: ["RM1", "Nome1",19, "Curso1", "Turma1"]}
 
 def total_alunos():
     for index, lista in matriculas.items():
@@ -13,5 +9,26 @@ def total_alunos():
         print("Curso:.........", lista[3])
         print("Turma:.........", lista[4])
 
-def cancel_matricula():
-    rm_remove = input("Digite o RM que deseja cancelar: ")
+def add_matricula():
+    id = list(matriculas.keys())
+    matriculas[id[len(id)-1]+1] = [input("Digite o RM:").title(), input("Digite o nome:"), int(input("Digite a idade do aluno:")), input("Digite o curso:"), input("Digite a Turma:")]
+    total_alunos()
+
+def remover_mat():
+    busca = input("Entre com o RM do aluno: ").title()
+    for index, lista in matriculas.items():
+        if busca == lista[0]:
+            del matriculas[index]
+            break
+    total_alunos()
+
+escolha = int(input("Escolha se deseja remover a matricula (1), adicionar (2) ou ver o total (3)"))
+
+if escolha == 3:
+    total_alunos()
+elif escolha == 2:
+    add_matricula()
+elif escolha == 1:
+    remover_mat()
+else:
+    print("opção inválida")
